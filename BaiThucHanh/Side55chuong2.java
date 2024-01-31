f04f2bee8cc26d0bce33ca41af1ce2052f5fbd99
vuviethoang414 <hoangvu04012004@gmail.com>
2024-01-31 19:15:55 +0700

    Side 55 chuong 2

---
+++ BaiThucHanh/Side55chuong2.java
@@ -0,0 +1,14 @@
+/**
+ * Slide55chuong2
+ */
+public class Side55chuong2 {
+
+    public static void main(String[] args) {
+        System.out.println("Cac so chia het cho 5 la: ");
+        for(int i = 0; i<=20; i++)
+            if(i % 5 == 0){
+            System.out.println(i);
+            }       
+    }
+}
+
