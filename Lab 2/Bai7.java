77f68edb476c634fb9eba8521d401ea519725f8d
vuviethoang414 <hoangvu04012004@gmail.com>
2024-02-02 09:51:16 +0700

    Bai7 Lab2

---
+++ Lab 2/Bai7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Bai7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("20 so  nguyen dau tien :");
+        for(int i=0;i<20;i++){
+            System.out.println(+i);
+        }
+    }
+    
+}
