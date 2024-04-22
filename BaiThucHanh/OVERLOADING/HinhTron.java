ecaec222b3600151d8605ad9bbc168c377d442fe
vuviethoang414 <hoangvu04012004@gamil.com>
2024-04-22 23:27:02 +0700

    Side78 Chuong3

---
+++ BaiThucHanh/OVERLOADING/HinhTron.java
@@ -0,0 +1,16 @@
+package OVERLOADING;
+import STATIC.HinhHoc;
+public class HinhTron extends HinhHoc {
+    //Khong tham so truen vao 
+    // ung dung phai goi cac phuon thuc nhap ban kinh va don vi console
+    public void nhapBanKinh(){
+    }
+    //Co mot tham so truyen vao la ban kinh,
+    // ung dung phai goi them phuong thuc nhap don vi tu console
+    public void nhapBanKinh(float bankinh){
+    }
+    //Co hai tham so truyen vao la ban kinh va don vi
+    //Ung dung chi viec gan hai gia tri nay vao cac thuoc tinh tuong ung
+    public void nhapBanKinh(float bankinh,int donVi){
+}
+}
