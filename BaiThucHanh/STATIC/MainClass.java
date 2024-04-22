e22d53ca9f01dc56eaa7f58876afbcde56f624c6
vuviethoang414 <hoangvu04012004@gamil.com>
2024-04-22 22:18:41 +0700

    Side 73 Chuong 3

---
+++ BaiThucHanh/STATIC/MainClass.java
@@ -0,0 +1,16 @@
+package STATIC;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ToaDo td1 = new ToaDo();
+        td1.x=10;
+        td1.y=20;
+
+        ToaDo td2 = new ToaDo();
+        td2.x=5;
+        td2.y=6;
+
+        ToaDo.thongTin="Luu toa do cac hinh hoc";
+
+    }
+}
---
+++ BaiThucHanh/STATIC/ToaDo.java
@@ -0,0 +1,8 @@
+package STATIC;
+
+public class ToaDo {
+    public static String thongTin;
+    public int x;
+    public int y;
+    }
+
