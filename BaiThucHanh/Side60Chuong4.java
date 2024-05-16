2bd3029b55aed41ac5c0f89e718e15973e898216
vuviethoang414 <hoangvu04012004@gamil.com>
2024-05-17 01:18:28 +0700

    Side60 Chuong4

---
+++ BaiThucHanh/Side60Chuong4.java
@@ -0,0 +1,31 @@
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Side60Chuong4 {
+    public static void main(String[] args) {
+        String a;
+        Scanner sc = new Scanner(System.in);
+        Set<String> linkedHashSet = new LinkedHashSet<String>();
+        linkedHashSet.add("JAVA");
+        linkedHashSet.add("C++");
+        linkedHashSet.add("C#");
+        linkedHashSet.add("HTML");
+
+        System.out.println("Cac phan tu trong linkedHashSet: ");
+        System.out.println(linkedHashSet);
+        System.out.println("Phan tu can xoa: ");
+        a = sc.nextLine();
+
+        if(linkedHashSet.contains(a)) {
+            linkedHashSet.remove(a);
+            System.out.println("Cac phan tu trong linkedHashSet sau khi xoa la: ");
+            System.out.println(linkedHashSet);
+        } else {
+            System.out.println("Phan tu " + a + " khong ton tai! ");
+        }
+    }
+    
+}
+
